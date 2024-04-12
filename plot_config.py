e40a136fd4e1f6cc41366f21435df310ca7ada51
Dubidu1212 <rz.ausserschwand@gmail.com>
2024-04-12 18:26:46 +0200

    changed a bit in plotconfig (but it's possibly still broken'

--- plot_config.py
+++ plot_config.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib import rc, rcParams, cm
 # global settings of plots DO NOT CHANGE
 medium_fontsize = 20.5
+rcParams['text.latex.preamble']= r"""
+\usepackage{amsmath}
+\boldmath
+"""
 font = {'size': medium_fontsize, 'family': 'sans-serif', 'weight': 'bold'}
 rc('font', **font)
 rcParams['axes.linewidth'] = 1.5
@@ -11,3 +15,4 @@ rcParams['xtick.major.width'] = 1.5
 rcParams['ytick.major.width'] = 1.5
 rcParams['lines.linewidth'] = 2.5
 rcParams['figure.figsize'] = (8, 8)
+rc('text', usetex=True)
