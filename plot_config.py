d1e2fd61cadf4dcda9fdb8104211d8dc7159c98d
Dubidu1212 <rz.ausserschwand@gmail.com>
2024-04-12 17:10:01 +0200

    added plot_config

---
+++ plot_config.py
@@ -0,0 +1,13 @@
+import sympy as sp 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc, rcParams, cm
+# global settings of plots DO NOT CHANGE
+medium_fontsize = 20.5
+font = {'size': medium_fontsize, 'family': 'sans-serif', 'weight': 'bold'}
+rc('font', **font)
+rcParams['axes.linewidth'] = 1.5
+rcParams['xtick.major.width'] = 1.5
+rcParams['ytick.major.width'] = 1.5
+rcParams['lines.linewidth'] = 2.5
+rcParams['figure.figsize'] = (8, 8)
