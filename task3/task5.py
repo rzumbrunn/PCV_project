d8667f600f6531e95d5173bda5a51b4d01318a27
daniisler <isler.dani@gmx.ch>
2024-05-18 16:52:44 +0200

    integrated changes from jupiter notebook

--- task3/task5.py
+++ task3/task5.py
@@ -4,7 +4,13 @@ sys.path.append('../')
 sys.path.append('./PCV_project')
 from plot_config import *
 
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+# %%
+# Simulation methods
 
 def euler_sim(M, x0 ,dt, T, Perturbation=None):
     t_range = np.linspace(0, T, int(T/dt))[:-1]
@@ -58,7 +64,17 @@ def implicit_euler_sim(M,x0,dt , T, Perturbation = None):
     return X
 
 # %%
-#constants
+# Definitions of the laser -> coupling of the states
+
+def intensity(t):
+    return 2*E/(tau_p * np.pi* w_0**2)*2**(-4*((t-t_0)/(tau_p))**2)
+
+def Omega_if(t, mu):
+    return mu*np.sqrt(intensity(t))
+
+# %%
+# Constants
+
 from scipy.constants import physical_constants as pc
 aut = pc['atomic unit of time'][0]
 E = 200*1e-6 / pc['atomic unit of energy'][0] #uJ
@@ -75,44 +91,49 @@ E_i_phi = 5000*1e2 * pc["atomic unit of length"][0]  #cm^-1
 E_i_psi = 5000*1e2 * pc["atomic unit of length"][0] #cm^-1
 Gamma_diss = 25*1e2 * pc["atomic unit of length"][0] #cm^-1
 
-#
-
-
-# %%
-def intensity(t):
-    return 2*E/(tau_p * np.pi* w_0**2)*2**(-4*((t-t_0)/(tau_p))**2)
-
-def Omega_if(t, mu):
-    return mu*np.sqrt(intensity(t))
-
-# %%
-import numpy as np
-import matplotlib.pyplot as plt
-
 x0 = np.array([1, 0, 0])
-dt = 0.000001e-9/aut
+dt = 0.005e-9/aut
 T = 50e-9/aut
 
-def calc_dissoc_yield(omega, q):
-    E_i = -omega
+omega_range = np.linspace(0.7*E_i_phi, 1.1*E_i_phi, 14)
+num_threads_max = 7
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(''))
+X_cache_dir = os.path.join(PROJECT_DIR, 'task3', 'x_cache')
+os.makedirs(X_cache_dir, exist_ok=True)
+
+
+# %%
+import h5py
+
+def calc_dissoc_yield(omega):
+    # Check if the file is already computed
+    if os.path.isfile(os.path.join(X_cache_dir, f'X_{str(omega*1e10)}_dt_{round(dt)}.h5')):
+        with h5py.File(os.path.join(X_cache_dir, f'X_{str(omega*1e10)}_dt_{round(dt)}.h5'), 'r') as hf:
+            X = hf['X'][:]
+        dissoc_yield = 1 - np.max(np.abs(X[-10000:-1,0])**2)
+        print(f'Loaded from cache: omega = {omega}, dissoc_yield = {dissoc_yield}')
+        return omega, dissoc_yield
+
+    E_i = 0
     E_phi = E_i_phi - omega
     E_psi = E_i_psi - omega + 1j* Gamma_diss/2
     g_phipsi = 0
 
-    #laser
+    # laser
     laser = lambda t: np.array(
         [
-            [0, np.conjugate(Omega_if(t,mu_i_phi)), np.conjugate(Omega_if(t,mu_i_psi))],
-            [Omega_if(t,mu_i_phi), 0, 0],
-            [Omega_if(t,mu_i_psi), 0, 0],
+            [0, np.conjugate(Omega_if(t,mu_i_phi))/2, np.conjugate(Omega_if(t,mu_i_psi))/2],
+            [Omega_if(t,mu_i_phi)/2, 0, 0],
+            [Omega_if(t,mu_i_psi)/2, 0, 0],
         ]
     )
 
     M = np.array(
         [
             [E_i, 0,0],
-            [0, E_phi, np.conjugate(g_phipsi)],
-            [0, g_phipsi, E_psi],
+            [0, E_phi, np.conjugate(g_phipsi)/2],
+            [0, g_phipsi/2, E_psi],
         ]
     )
 
@@ -120,67 +141,45 @@ def calc_dissoc_yield(omega, q):
 
     X = rk4_sim(M, x0, dt, T, Perturbation)
 
-    q.put(X)
+    # Save with HDF5 (better than .txt for large arrays)
+    with h5py.File(os.path.join(X_cache_dir, f'X_{str(omega*1e10)}_dt_{round(dt)}.h5'), 'w') as hf:
+        hf.create_dataset('X', data=X)
+    dissoc_yield = 1 - np.max(np.abs(X[-10000:-1,0])**2)
+    return omega, dissoc_yield
+
+
+def worker(omega, result_dict):
+    result_dict[omega] = calc_dissoc_yield(omega)
 
 
 # %%
-from multiprocessing import Process, Queue
-import os
-PROJECT_DIR = os.path.abspath('')
-X_cache_dir = os.path.join('/scratch/islerd/x_cache')
-os.makedirs(X_cache_dir, exist_ok=True)
+# Parallel processing
 
-threads = []
-omega_range = np.linspace(-1.25*E_i_phi, 1.25*E_i_phi, 66)
-# Dictionary with omega_range values as keys
-dissoc_yields = {omega: None for omega in omega_range}
-for omega in omega_range:
-    q = Queue()
-    p = Process(target=calc_dissoc_yield, args=(omega,q,))
-    threads.append((p, q, omega))
+dissoc_yields = {}
 
-num_threads_max = 22
-num_threads_running = 0
-running_threads = []
+with ProcessPoolExecutor(max_workers=num_threads_max) as executor:
+    futures = {executor.submit(calc_dissoc_yield, omega): omega for omega in omega_range}
+    for future in as_completed(futures):
+        omega, dissoc_yield = future.result()
+        dissoc_yields[omega] = dissoc_yield
+        print(f'Finished for omega/E_i_phi={omega/E_i_phi}, dissoc_yield={dissoc_yield}')
 
 
-first = True
-while len(threads) > 0:
-    if num_threads_running >= num_threads_max or len(threads) == 0:
-        # Wait until a thread finishes
-        for thread in running_threads:
-            # dissoc_yield = thread[1].get()
-            X = thread[1].get()
-            # Store X to disk
-            np.savetxt(os.path.join(X_cache_dir, f'X_{str(thread[2]*1e10)}_dt_{round(dt)}.txt'), X)
-            dissoc_yield = 1 - np.max(np.abs(X[-10000:-1,0])**2)
-            print(f'Saved for omega={thread[2]}')
-            thread[0].join()
-            dissoc_yields[thread[2]] = dissoc_yield
-            running_threads.remove(thread)
-            num_threads_running -= 1
-    else:
-        for i in range(num_threads_max - num_threads_running):
-            if len(threads) == 0:
-                break
-            thread = threads.pop(0)
-            thread[0].start()
-            print(f"Thread for omega={thread[2]} started")
-            num_threads_running += 1
-            running_threads.append(thread)
 
-# Wait for the remaining threads to finish
-for thread in running_threads:
-    X = thread[1].get()
-    # Store X to disk
-    np.savetxt(os.path.join(X_cache_dir, f'X_{str(thread[2]*1e10)}_dt_{round(dt)}.txt'), X)
-    dissoc_yield = 1 - np.max(np.abs(X[-10000:-1,0])**2)
-    print(f'Saved for omega={thread[2]}')
-    thread[0].join()
-    dissoc_yields[thread[2]] = dissoc_yield
+# %%
+# t = np.linspace(0, T, int(T/dt))
+# print(X.shape)
+# plot_range = [999000, 999200]
+# plt.plot(t[plot_range[0]:plot_range[1]], np.real(X[:, 0][plot_range[0]:plot_range[1]]), label="Real")
+# plt.plot(t[plot_range[0]:plot_range[1]], np.imag(X[:, 0][plot_range[0]:plot_range[1]]), label="Imag")
+# plt.show()
 
 # %%
 # Plot the results
-plt.plot(omega_range, list(dissoc_yields.values()))
+plt.plot([float(omega)/E_i_phi for omega in dissoc_yields.keys()], list(dissoc_yields.values()), ls='', marker='o')
 plt.savefig(os.path.join(PROJECT_DIR, 'fano.png'))
+plt.xlabel(r"$\omega$ (E_i_phi)")
+plt.ylabel("Dissociation Yield")
 print('Finished successfully')
+
+
